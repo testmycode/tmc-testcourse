1551895427f8a9ca220ded0f881483a0c3174742
Martin Pärtel <martin.partel@gmail.com>
2012-08-06 17:46:48 +0300

    Added sample Maven exericse.

---
+++ maven_exercise/src/main/java/fi/helsinki/cs/maventest/App.java
@@ -0,0 +1,10 @@
+package fi.helsinki.cs.maventest;
+
+public class App {
+    public static void main(String[] args) {
+        //STUB://System.out.println("Hello Maven!");
+        //BEGIN SOLUTION
+        System.out.println("Hello Maven!");
+        //END SOLUTION
+    }
+}
