88360b4dc21e8f6f269e981d2710c98fda4d67f9
Jarmo Isotalo <jamo@isotalo.fi>
2015-02-22 17:12:41 +0100

    Added makefile sample project

---
+++ makefile/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "source.h"
+
+int main()
+{
+    three_lines();
+    return 0;
+}
---
+++ makefile/src/source.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+#include "source.h"
+
+void three_lines(void)
+{
+    printf("January\nFebruary\nMarch\n");
+}
