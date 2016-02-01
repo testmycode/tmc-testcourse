82d39212667549742b2976dc0e1be9129759cf46
ljleppan <leo.leppanen@helsinki.fi>
2016-02-01 12:00:17 +0200

    Rename and fix test_arithfuncs.py
    Renamed arthfuncs_test.py -> test_arithfuncs.py for one less typo and so that unittest actually discovers
    the test.
    
    Also reflected the removal of the class in the test code.

--- python3_simple/test/test_arithfuncs.py
+++ python3_simple/test/test_arithfuncs.py
@@ -4,14 +4,14 @@ from tmc import points
 
 from tmc.utils import load, get_out
 
-Arithfuncs = load('src.arithfuncs', 'sum')
+sum = load('src.arithfuncs', 'sum')
 
 @points('python3_1')
 class TestCase(unittest.TestCase):
 
     @points('py3')
     def test_new(self):
-        self.assertEqual(sum(1,2), 3);
+        self.assertEqual(sum(1, 2), 3);
 
 if __name__ == '__main__':
     unittest.main()
