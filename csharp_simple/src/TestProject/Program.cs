5b7bffa70954bcb5f48517789ad8cc1e9befe2af
Tubaias <topias_t@hotmail.com>
2020-05-15 13:27:04 +0300

    couple extra functions to test

--- csharp_simple/src/TestProject/Program.cs
+++ csharp_simple/src/TestProject/Program.cs
@@ -6,6 +6,16 @@ namespace TestProject
     {
         public static bool ReturnTrue => true;
 
+        public static bool ReturnNotInput(bool input)
+        {
+            return !input;
+        }
+
+        public static string ReturnInputString(string input)
+        {
+            return input;
+        }
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello Home");
--- csharp_simple/test/TestProjectTests/ProgramTest.cs
+++ csharp_simple/test/TestProjectTests/ProgramTest.cs
@@ -1,15 +1,33 @@
 using System;
 using Xunit;
 using TestProject;
+using TestMyCode.Csharp.API.Attributes;
 
 namespace TestProjectTests
 {
     public class ProgramTest
     {
         [Fact]
+        [Points("1.3")]
         public void TestReturnsTrue()
         {
             Assert.True(Program.ReturnTrue);
+        }
+
+        [Fact]
+        [Points("0.5")]
+        public void ReturnsNotInput()
+        {
+            Assert.True(Program.ReturnNotInput(false));
+            Assert.False(Program.ReturnNotInput(true));
+        }
+
+        [Fact]
+        [Points("2.1")]
+        public void ReturnsString()
+        {
+            string s = "asd";
+            Assert.Equal(s, Program.ReturnInputString(s));
         }
     }
 }
