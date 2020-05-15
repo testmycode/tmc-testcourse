a5027ecb5c3a18cb74a1226a53be25ef23b52687
Tubaias <topias_t@hotmail.com>
2020-05-15 13:31:31 +0300

    deleted points for now

--- csharp_simple/test/TestProjectTests/ProgramTest.cs
+++ csharp_simple/test/TestProjectTests/ProgramTest.cs
@@ -1,21 +1,18 @@
 using System;
 using Xunit;
 using TestProject;
-using TestMyCode.Csharp.API.Attributes;
 
 namespace TestProjectTests
 {
     public class ProgramTest
     {
         [Fact]
-        [Points("1.3")]
         public void TestReturnsTrue()
         {
             Assert.True(Program.ReturnTrue);
         }
 
         [Fact]
-        [Points("0.5")]
         public void ReturnsNotInput()
         {
             Assert.True(Program.ReturnNotInput(false));
@@ -23,7 +20,6 @@ namespace TestProjectTests
         }
 
         [Fact]
-        [Points("2.1")]
         public void ReturnsString()
         {
             string s = "asd";
